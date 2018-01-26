57fa4829c6aad6ab34378d7d4595a9db28e1896b
scott <gitpushoriginmaster@gmail.com>
2018-01-26 10:55:37 -0600

    adding cifar10 model variables for grid search

--- src/cifar10_models.py
+++ src/cifar10_models.py
@@ -39,6 +39,10 @@ class Cifar10ShallowConvolutionalModel(TrainableMixin):
                 self.b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]), name="bias", trainable=trainable)
                 self.h_conv1 = tf.nn.relu(conv2d(self.x_image, self.W_conv1) + self.b_conv1, name="relu")
                 self.h_pool1 = max_pool_2x2(self.h_conv1, name="max_pool")
+
+                self.weight_decay_layer_1 = tf.Variable(0,trainable=False,name="weight_decay_rate")
+                self.layer_1_loss = tf.multiply(tf.nn.l2_loss(self.W_conv1), self.weight_decay_layer_1, name="weight_loss")
+
                 layer_1_out_dim = [int(layer_1_in_dim[0]/2), int(layer_1_in_dim[1]/2), 32]
 
             with tf.name_scope("layer_2_conv"):
@@ -47,6 +51,10 @@ class Cifar10ShallowConvolutionalModel(TrainableMixin):
                 self.b_conv2 = tf.Variable(tf.constant(0.1,shape=[64]), name="bias", trainable=trainable)
                 self.h_conv2 = tf.nn.relu(conv2d(self.h_pool1, self.W_conv2) + self.b_conv2, name="relu")
                 self.h_pool2 = max_pool_2x2(self.h_conv2, name="max_pool")
+
+                self.weight_decay_layer_2 = tf.Variable(0,trainable=False,name="weight_decay_rate")
+                self.layer_2_loss = tf.multiply(tf.nn.l2_loss(self.W_conv2), self.weight_decay_layer_2, name="weight_loss")
+
                 layer_2_out_dim = [int(layer_2_in_dim[0]/2), int(layer_2_in_dim[1]/2), 64]
 
             with tf.name_scope("layer_3_conv"):
@@ -55,6 +63,10 @@ class Cifar10ShallowConvolutionalModel(TrainableMixin):
                 self.b_conv3 = tf.Variable(tf.constant(0.1,shape=[128]), name="bias", trainable=trainable)
                 self.h_conv3 = tf.nn.relu(conv2d(self.h_pool2, self.W_conv3) + self.b_conv3, name="relu")
                 self.h_pool3 = max_pool_2x2(self.h_conv3, name="max_pool")
+
+                self.weight_decay_layer_3 = tf.Variable(0,trainable=False,name="weight_decay_rate")
+                self.layer_3_loss = tf.multiply(tf.nn.l2_loss(self.W_conv2), self.weight_decay_layer_3, name="weight_loss")
+
                 layer_3_out_dim = [int(layer_3_in_dim[0]/2), int(layer_3_in_dim[1]/2), 128]
 
             with tf.name_scope("layer_4_fc"):
@@ -64,22 +76,23 @@ class Cifar10ShallowConvolutionalModel(TrainableMixin):
                 self.W_fc1 = tf.Variable(tf.truncated_normal(layer_4_in_flat_dim+[1024], stddev=0.1), name="weights", trainable=trainable)
                 self.b_fc1 = tf.Variable(tf.constant(0.1,shape=[1024]), name="bias", trainable=trainable)
                 self.h_fc1 = tf.nn.relu(tf.matmul(self.in_flat_fc1, self.W_fc1) + self.b_fc1, name="relu")
-                self.drop_fc1 = tf.placeholder_with_default(1.0, [], "dropout_rate")
+                self.drop_fc1 = tf.Variable(1.0, name="dropout_rate",trainable=False)
                 self.h_fc1_drop = tf.nn.dropout(self.h_fc1, self.drop_fc1, name="dropout")
                 layer_4_out_dim = [1024]
 
             with tf.name_scope("layer_5_fc"):
                 layer_5_in_dim = layer_4_out_dim
-                #self.W_fc2 = tf.Variable(tf.truncated_normal(layer_5_in_dim + [10], stddev=0.1), name="weights", trainable=trainable)
-                #self.b_fc2 = tf.Variable(tf.constant(0.1,shape=[10]), name="bias", trainable=trainable)
-                self.W_fc2 = tf.Variable(tf.truncated_normal(layer_5_in_dim + [10], stddev=0.1), trainable=trainable)
-                self.b_fc2 = tf.Variable(tf.constant(0.1,shape=[10]), trainable=trainable)
+                self.W_fc2 = tf.Variable(tf.truncated_normal(layer_5_in_dim + [10], stddev=0.1), name="weights", trainable=trainable)
+                self.b_fc2 = tf.Variable(tf.constant(0.1,shape=[10]), name="bias", trainable=trainable)
                 self.logits = tf.matmul(self.h_fc1_drop, self.W_fc2) + self.b_fc2
                 layer_5_out_dim = [10]
 
             with tf.name_scope("outputs"):
                 self.proba = tf.nn.softmax(self.logits, name="proba")
-                self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.logits), name="loss")
+                self.weight_decay_layer_2 = tf.Variable(0,trainable=False)
+                self.weight_decay_layer_3 = tf.Variable(0,trainable=False)
+                self.cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.logits), name="cross_entropy_loss")
+                
       
             with tf.name_scope("summaries"):
                 self.correct_prediction = tf.equal(tf.argmax(self.logits,1), tf.argmax(self.y_,1))
